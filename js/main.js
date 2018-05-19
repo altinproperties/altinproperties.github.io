2a4d267719c8bee3935e8d0ac120bc8d42dc975a
moonlandingnetwork <38373420+moonlandingnetwork@users.noreply.github.com>
2018-05-19 16:10:23 +0530

    Layout Revision

--- js/main.js
+++ js/main.js
@@ -31,6 +31,24 @@
 [ End table content ]
 ======================================*/
 
+jQuery(document).ready(function () {
+    function openFancybox() {
+        setTimeout(function () {
+            jQuery('#popuplink').trigger('click');
+        }, 500);
+    };
+    var visited = jQuery.cookie('visited');
+    if (visited == 'yes') {
+        openFancybox();
+         // second page load, cookie active
+    } else {
+        openFancybox(); // first page load, launch fancybox
+    }
+    jQuery.cookie('visited', 'yes', {
+        expires: 365 // the number of days cookie  will be effective
+    });
+    jQuery("#popuplink").fancybox({modal:true, maxWidth: 600, overlay : {closeClick : true}});
+});
 
 (function($) {
     "use strict";
